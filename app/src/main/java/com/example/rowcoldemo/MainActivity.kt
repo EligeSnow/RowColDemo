7c3e897fe3d3c3cef1476ed45f03b071f4528eef
EligeSnow <ilyaklobertants398@gmail.com>
2026-09-21 22:11:29 +0700

    Расположение элементов макета

--- app/src/main/java/com/example/rowcoldemo/MainActivity.kt
+++ app/src/main/java/com/example/rowcoldemo/MainActivity.kt
@@ -35,7 +35,7 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun MainScreen(modifier: Modifier = Modifier) {
-    Row(verticalAlignment = Alignment.CenterVertically,
+    Row(horizontalArrangement = Arrangement.End,
         modifier = modifier.size(width = 400.dp, height = 200.dp)) {
         TextCell("1")
         TextCell("2")
